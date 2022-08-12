7efdf57424c1c130aa5c0c7654b33d79fd78f8e3
Tardz <jonathan.almstedt@live.se>
2022-08-12 02:06:56 +0200

    upload

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -129,15 +129,15 @@ layouts = [
         border_focus=gruvbox['windowfocus1'],
         border_width=2,
         num_stacks=1,
-        margin=6,
+        margin=10,
     ),
     MonadTall(
         border_normal=gruvbox['windowoutline'],
         border_focus=gruvbox['windowfocus0'],
-        margin=6,
+        margin=10,
         border_width=2,
         single_border_width=2,
-        single_margin=6,
+        single_margin=10,
     )
 ]
 
