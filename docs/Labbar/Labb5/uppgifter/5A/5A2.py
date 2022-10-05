18b318f7559fd249568e27db3b8ab8930b1cbb06
Tardz <jonathan.almstedt@live.se>
2022-10-05 21:23:46 +0200

    upload

--- docs/Labbar/Labb5/uppgifter/5A/5A2.py
+++ docs/Labbar/Labb5/uppgifter/5A/5A2.py
@@ -5,13 +5,26 @@ import numpy as np
 import cv2 as cv
 
 def G(x, y, s = 4.5):
-    return ((-(1//(2*np.pi*s**2)))*np.exp(-((x**2)+(y**2))//(2*s**2)))
+    if x == y == 0:
+        return 1.5
+    else:
+        return "{:.6f}".format((-(1/(2*np.pi*s**2)))*np.exp(-(((x**2)+(y**2))/(2*s**2))))
 
 def unsharp_mask(N):
     """"""
-    N_index = round((N//2)*-1)
+    array = []
+    y = round((N//2)*-1)
+    x = y - 1
+    for ele in range(N):
+        x =+ 1
+        for sub_ele in range(N):
+            array.append([G(x, y), G(x, y+1), G(x, y+2)])
+    
+    print("Array length:", len(array))
+    return array
 
-    array = [ele = for ele in range(N)]
 
 print(unsharp_mask(3))
-
+# 
+# array = [[x for x in range(2)] for y in range(N*N)]
+print(G(0,-1))
